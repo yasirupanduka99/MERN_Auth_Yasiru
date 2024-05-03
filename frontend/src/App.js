dcf5dc5bdcd5081ef6b4a1a9c039c7aca92b4a91
Yasiru Panduka <it20162382@my.sliit.lk>
2024-05-03 17:06:56 +0530

    Protecting React Routes 🍟😁

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,50 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // import pages
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 // import components
-import CreateForm from './components/CreateForm';
-import UpdateForm from './components/UpdateForm';
-import AllItems from './components/AllItems';
-import NavBar from './components/NavBar';
-
+import CreateForm from "./components/CreateForm";
+import UpdateForm from "./components/UpdateForm";
+import AllItems from "./components/AllItems";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
-        
       <BrowserRouter>
+        <NavBar />
 
-      <NavBar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />} // if user logged in show the Home component and if user not logged in then navigate to login page.
+            />
 
-      <div className='pages'>
-        <Routes>
+            <Route
+              exact
+              path="/signup"
+              element={!user ? <SignUp /> : <Navigate to="/" />} // if user not logged in then show Signup componnent and if user logged in then navigate to Home component.
+            />
+            <Route
+              exact
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />} // if user not logged in then show Login componnent and if user logged in then navigate to Home component.
+            />
 
-          <Route path='/' element={ <Home /> } />
-
-          <Route exact path="/signup" element={<SignUp/>} />
-          <Route exact path="/login" element={<Login/>} />
-
-          <Route path='/createform' element={<CreateForm/>} />
-          <Route path='/updateform/:id' element={<UpdateForm/>} />
-          <Route path='/allitems' element={<AllItems/>} />
-
-        </Routes>
-      </div>
-        
+            <Route path="/createform" element={<CreateForm />} />
+            <Route path="/updateform/:id" element={<UpdateForm />} />
+            <Route path="/allitems" element={<AllItems />} />
+          </Routes>
+        </div>
       </BrowserRouter>
-
     </div>
   );
 }
